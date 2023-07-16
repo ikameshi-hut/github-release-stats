96f622c4b8004e9b78aecd4d20743c597cce24d5
ikameshi-hut <138295028+ikameshi-hut@users.noreply.github.com>
2023-07-16 11:06:53 +0900

    English sentence modified

--- js/langchange.js
+++ js/langchange.js
@@ -3,18 +3,10 @@ let len = elements.length;
 let lang;
 
 // --- ブラウザのデフォルト言語を取得して初回の表示 ----- 
-lang = (navigator.browserLanguage || navigator.language || navigator.userLanguage).substr(0,2);
-
-for (let i = 0; i < len; i++){
-    elements[i].checked = false;
-}
-
-// Set browser's lang setting
-if (lang == "ja") elements[0].checked = true;
-else if (lang == "en") elements[1].checked = true;
-
+langSet((navigator.browserLanguage || navigator.language || navigator.userLanguage).substr(0,2));
 
 // If another lang was selected, change variable, too
+// Called when radio button clicked
 function langSet(argLang){
     lang = argLang;
 
